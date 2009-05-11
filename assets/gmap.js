7b22f4902131ba316cbf83d738e94b5ca18c9906
Scott Hughes <Scott@Scott.local>
2009-05-11 21:16:09 +1000

    Fixed issues related to converting to jQuery

--- assets/gmap.js
+++ assets/gmap.js
@@ -1,25 +1,33 @@
-UIControl.deploy("label.GMap input", function(input) {
-	var coordinates = input.value.match(/[-\d.]+/g) || [37.4419, -122.1419], // default location
-	    container   = DOM.insertElement("span", input.parentNode);
+jQuery(function() {
+	jQuery('label.GMap input').each(function() {
 
-	input.type = "hidden";
+		var coordinates = this.value.match(/[-\d.]+/g) || [37.4419, -122.1419], // default location
+		    container   = this.parentNode.appendChild(document.createElement('span')),
+		    replacement = document.createElement('input');
 
-	container.style.padding = 0;
-	container.style.height  = Math.round(0.6 * container.offsetWidth) + "px";
+		replacement.type  = 'hidden';
+		replacement.name  = this.name;
+		replacement.value = this.value;
 
-	var map      = new GMap2(container),
-	    center   = new GLatLng(coordinates[0], coordinates[1]),
-	    marker   = new GMarker(center, {draggable: true}),
-		geocoder = new GClientGeocoder();
+		this.parentNode.replaceChild(replacement, this);
+
+		container.style.padding = 0;
+		container.style.height  = Math.round(0.6 * container.offsetWidth) + "px";
+
+		var map      = new GMap2(container),
+		    center   = new GLatLng(coordinates[0], coordinates[1]),
+		    marker   = new GMarker(center, {draggable: true}),
+			geocoder = new GClientGeocoder();
 		
-	map.setCenter(center, 13);
-	map.addControl(new GSmallMapControl());
-	map.addOverlay(marker);
+		map.setCenter(center, 13);
+		map.addControl(new GSmallMapControl());
+		map.addOverlay(marker);
 
-	DOM.Event.addListener(input.form, "submit", function() {
-		var point = marker.getLatLng();
-		input.value = point.lat() + ", " + point.lng();
+		jQuery(replacement.form).submit(function() {
+			var point = marker.getLatLng();
+			replacement.value = point.lat() + ", " + point.lng();
+		});
+
+		jQuery(window).bind("unload", GUnload);
 	});
-
-	DOM.Event.addListener(window, "unload", GUnload);
 });
